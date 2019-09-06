0f1425541c35b98cbdc3b84b63d80dab1e6ea988
ASITHAMUMNAKP <47180934+ASITHAMUMNAKP@users.noreply.github.com>
2019-09-06 17:23:25 +0530

    change

--- digital.js
+++ digital.js
@@ -1,10 +1,53 @@
-$(function () {
+// $(function () {
 
-     $(".screen2").hide();
-    $(".icon").click(function () {
-        $(".screen1").hide();
+//      $(".screen2").hide();
+//      $(".screen3").hide();
+//      $(".screen4").hide();
+//      $(".screen5").hide();
+//     $(function ()
+//     {
+//      $(".mes").click(function () {
+//         $(".screen1").hide();
+//          $(".screen3").hide();
+//          $(".screen4").hide();
+//          $(".screen5").hide();
+//          $(".screen2").show(); 
+//      });
+// });
+// });
+window.onload = () => {
+    let hour = 0;
+    let minute = 0;
+    let seconds = 0;
+    let totalSeconds = 0;
+
+    let intervalId = 0;
+
+    function startTimer() {
+        ++totalSeconds;
+        hour = Math.floor(totalSeconds / 3600);
+        minute = Math.floor((totalSeconds - hour * 3600) / 60);
+        seconds = totalSeconds - (hour * 3600 + minute * 60);
+
+        document.getElementById("hour").innerHTML = hour;
+        document.getElementById("min").innerHTML = minute;
+        document.getElementById("sec").innerHTML = seconds;
+    }
+
+    document.getElementById('start').addEventListener('click', () => {
+        intervalId = setInterval(startTimer, 1000);
+    })
+
+    document.getElementById('stop').addEventListener('click', () => {
+        if (intervalId != 0)
+            clearInterval(intervalId);
     });
-     $(".icon").click(function () {
-         $(".screen2").show();
-     });
-});
+
+
+    document.getElementById('reset').addEventListener('click', () => {
+        totalSeconds = 0;
+        document.getElementById("hour").innerHTML = '0';
+        document.getElementById("min").innerHTML = '0';
+        document.getElementById("sec").innerHTML = '0';
+    });
+}
